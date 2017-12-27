8d65b543159c473b08ce58960f1b40572a18af93
Christopher Dorst <chris@dorst.email>
2017-12-27 12:35:21 -0600

    Adding .Prepend helper

---
+++ Common.Functions.PrependEnumerationElement/EnumerationElementPrepender.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static Common.Functions.CheckNullableEnumerationForAnyElements.NullableEnumerationAny;
+using static Common.Functions.CreateNewArrayFromSingleElement.ArrayCreator;
+
+namespace Common.Functions.PrependEnumerationElement
+{
+    public static class EnumerationElementPrepender
+    {
+        public static IEnumerable<T> Prepend<T>(T firstElement, IEnumerable<T> remainingElements)
+            => Any(remainingElements)
+                ? remainingElements.Prepend(firstElement)
+                : Array(firstElement);
+    }
+}
